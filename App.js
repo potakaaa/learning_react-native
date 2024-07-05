1330c138343fce6d3c2def7b15e9093dca04575f
potakaaa <helbirog@gmail.com>
2024-07-05 17:24:19 +0800

    added flexbox parent-children

--- App.js
+++ App.js
@@ -9,30 +9,45 @@ import {
   from 'react-native';
 import { useDeviceOrientation } from  "@react-native-community/hooks"; 
 
-
-
 export default function App() {
   const orientation = useDeviceOrientation();
   return (
     <SafeAreaView style={[styles.container, {
       height: orientation == "portrait" ?  "30%" : "100%"
     }]}>
-      <Text style={{
-        textAlign: "center",
-        textAlignVertical: "center",
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        color: "white"
-      }}  adjustsFontSizeToFit={true}>This is a text</Text>
+      <View style={[flexStyle, {backgroundColor: "red"}]}> 
+        <Text style={textStyle}  adjustsFontSizeToFit={true} >I love my girlfriend</Text>
+      </View>
+      <View style={[flexStyle, {backgroundColor: "gold"}]}>
+        <Text style={textStyle}  adjustsFontSizeToFit={true} >I love my girlfriend</Text>
+      </View>
+      <View style={[flexStyle, {backgroundColor: "dodgerblue"}]}>
+        <Text style={textStyle}  adjustsFontSizeToFit={true} >I love my girlfriend</Text>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: 'red',
     width: "100%",
     marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
+});
+
+const textStyle = StyleSheet.create({
+  textAlign: "center",
+  textAlignVertical: "center",
+  flex: 1,
+  alignItems: "center",
+  justifyContent: "center",
+  color: "white",
+  fontSize: 25,
+  fontWeight: "bold",
+});
+
+const flexStyle = StyleSheet.create({
+  flex: 1,
 });
